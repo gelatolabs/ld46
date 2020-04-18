c8b31b8bab253bd736e627841dc6a3c046d6305e
Kyle Farwell <m@kfarwell.org>
2020-04-18 15:16:08 -0400

    dos2unix

--- soundHandler.lua
+++ soundHandler.lua
@@ -1,11 +1,11 @@
 function setupSound()
 	titleMusic1 = love.audio.newSource("assets/music/title-intro.ogg", "stream")
-	titleMusic2 = love.audio.newSource("assets/music/title-loop.ogg", "stream")	
-	
+	titleMusic2 = love.audio.newSource("assets/music/title-loop.ogg", "stream")
+
 	titleMusic1:play()
 	currentMusic = "titlemusic1"
 end
-	
+
 function soundManager()
 	if gamePhase == "menu" then
 		if not titleMusic1:isPlaying() and not titleMusic2:isPlaying() then
