8f4c986aad968067b43834aaf070551352741c0b
Matthew Rose <mtrose42@gmail.com>
2020-04-19 19:26:21 -0400

    Added tutorial dialogue

--- dialogue.lua
+++ dialogue.lua
@@ -159,4 +159,136 @@ dialogue = {
 			},
 		},
 	},
+	["tutorial"] = {
+		[1] = {
+			["intro"] = "Narrator: Alright, now just let's see if you can get some food from this person.",
+			["success"] = "You know what, you seem like a good fella, have some timbits!",
+			["fail"] = "This should literally be unreachable, if you're seeing this then something went wrong. Like how did this even happen???",
+			["minscore"] = 1,
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "Attempt to steal food",
+						[2] = "Threaten with acts of extreme violence",
+						[3] = "Just straight up end their life",
+					},
+					["responses"] = {
+						[1] = "Narrator: Woah woah woah, what do you think you’re doing?!? You’re Canadian, you can’t just attack some random person! I don’t even think you guys are capable of that sort of thing. Nah, you gotta do this the Canadian way. Go on, try again.",
+						[2] = "Narrator: Woah woah woah, what do you think you’re doing?!? You’re Canadian, you can’t just attack some random person! I don’t even think you guys are capable of that sort of thing. Nah, you gotta do this the Canadian way. Go on, try again.",
+						[3] = "Narrator: Woah woah woah, what do you think you’re doing?!? You’re Canadian, you can’t just attack some random person! I don’t even think you guys are capable of that sort of thing. Nah, you gotta do this the Canadian way. Go on, try again.",
+					},
+					["outcomes"] = {
+						[1] = "negative",
+						[2] = "negative",
+						[3] = "negative",
+					},
+				},
+				[2] = {
+					["options"] = {
+						[1] = "Apologize",
+						[2] = "Compliment their hockey skates",
+						[3] = "Offer to build them a new fence",
+					},
+					["responses"] = {
+						[1] = "Oh, no problem at all, bud.",
+						[2] = "Oh, well thanks there, bud. That's real nice of you.",
+						[3] = "That's very kind, bud. That's okay though, I don't have a home anymore.",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "positive",
+						[3] = "positive",
+					},
+				},
+			},
+		},
+	},
+	["level1"] = {
+		[1] = {
+			["intro"] = "How's it going there, bro? Need something?",
+			["success"] = "Thanks for stopping to chat, bud. Have some poutine.",
+			["fail"] = "Can you quit bothering me, ya hoser? Can't ya see I'm busy.",
+			["minscore"] = 1,
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "Apologize",
+						[2] = "Insult their snowblower",
+						[3] = "Perform an immaculate moose call",
+					},
+					["responses"] = {
+						[1] = "Oh, no problem at all, bud.",
+						[2] = "You dare insult Delila, bud? ",
+						[3] = "That's a pretty impressive call you got there, bud.",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "negative",
+						[3] = "positive",
+					},
+				},
+				[2] = {
+					["options"] = {
+						[1] = "Offer to shine their skates",
+						[2] = "Apologize",
+						[3] = "Suggest they wear something other than plaid",
+					},
+					["responses"] = {
+						[1] = "What a nice offer, thanks bud.",
+						[2] = "Oh, not problem at all, bud.",
+						[3] = "You making a joke there, ya hoser?",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "positive",
+						[3] = "negative",
+					},
+				},
+			},
+		},
+	},
+	["template"] = {
+		[1] = {
+			["intro"] = "",
+			["success"] = "",
+			["fail"] = "",
+			["minscore"] = 1,
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "",
+						[2] = "",
+						[3] = "",
+					},
+					["responses"] = {
+						[1] = "",
+						[2] = "",
+						[3] = "",
+					},
+					["outcomes"] = {
+						[1] = "negative",
+						[2] = "negative",
+						[3] = "negative",
+					},
+				},
+				[2] = {
+					["options"] = {
+						[1] = "",
+						[2] = "",
+						[3] = "",
+					},
+					["responses"] = {
+						[1] = "",
+						[2] = "",
+						[3] = "",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "positive",
+						[3] = "positive",
+					},
+				},
+			},
+		},
+	},
 }
