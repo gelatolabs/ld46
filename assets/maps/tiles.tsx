59e1e0a0e8718b10cca997f0c8cb2e2a6e284bb1
Matthew Petry <firetwoonenine@gmail.com>
2020-04-20 17:21:36 -0500

    igloo map

---
+++ assets/maps/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="tiles" tilewidth="32" tileheight="32" tilecount="345" columns="15">
+ <image source="../tilesets/tiles.png" width="480" height="736"/>
+</tileset>
