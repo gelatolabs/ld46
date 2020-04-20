5250d143c097d4bf310b7cd309717a43d79e3363
Matthew Rose <mtrose42@gmail.com>
2020-04-19 21:08:38 -0400

    Add levels 3-5 dialogue

--- dialogue.lua
+++ dialogue.lua
@@ -350,6 +350,161 @@ dialogue = {
 			},
 		},
 	},
+	["level3"] = {
+		[1] = {
+			["intro"] = "Welcome to Pizza Town, bud. This is where they created the famed Hawaiian pizza! What can I do for ya?",
+			["success"] = "Here ya go bud, have a slice on the house!",
+			["fail"] = "Get out of my face, ya pineapple-hatin' hoser!",
+			["minscore"] = 1,
+			["food"] = "pizza",
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "Insult pineapple on pizza",
+						[2] = "Ask for a slice",
+						[3] = "Praise pineapple for the almighty fruit that it is",
+					},
+					["responses"] = {
+						[1] = "You dare come into Pizza town and utter such heresy?!",
+						[2] = "Well sure thing, bud.",
+						[3] = "Right you are, bud. Right you are.",
+					},
+					["outcomes"] = {
+						[1] = "negative",
+						[2] = "positive",
+						[3] = "positive",
+					},
+				},
+			},
+		},
+		[2] = {
+			["intro"] = "Hope you're liking Pizza Town, bud. What can I do for ya, eh?",
+			["success"] = "It's always nice to see a welcome face in Pizza Town",
+			["fail"] = "Never show your face in Pizza Town again, ya hoser!",
+			["minscore"] = 1,
+			["food"] = "pizza",
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "Compliment their toque",
+						[2] = "Offer to shovel their driveway",
+					},
+					["responses"] = {
+						[1] = "I'm only wearing this too hide my bald head...",
+						[2] = "I'm homeless, ya hoser, I don't have a driveway! How rude...",
+					},
+					["outcomes"] = {
+						[1] = "negative",
+						[2] = "negative",
+					},
+				},
+				[2] = {
+					["options"] = {
+						[1] = "Apologize",
+						[2] = "Explain that their favorite hockey player is in fact, the worst",
+					},
+					["responses"] = {
+						[1] = "Oh, don't worry aboot it bud.",
+						[2] = "You can fuck right off, eh, bud?",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "negative",
+					},
+				},
+			},
+		},
+	},
+	["level4"] = {
+		[1] = {
+			["intro"] = "Hey there bud, didn't expect to see someone so far North. Usually they stop in pizza town since they got an infinite supply of pizza. Oh well, ya can't go back now. What can I do for ya?",
+			["success"] = "Well gee, it's sure has been a while since I talked to a nice folk like yourself. Have this poutine.",
+			["fail"] = "I don't know about you, bud. Why don't you move along.",
+			["minscore"] = 2,
+			["food"] = "poutine",
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "Suggest a short game of hockey",
+						[2] = "Suggest that they go stuff their head in a snow bank",
+						[3] = "Compliment their snowmobile",
+					},
+					["responses"] = {
+						[1] = "I sure would love to, bud. Sadly I left my skates at home, maybe next time.",
+						[2] = "Well that's not very nice, ya hoser.",
+						[3] = "Thanks bud, I just polished her this morning.",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "negative",
+						[3] = "positive",
+					},
+				},
+				[2] = {
+					["options"] = {
+						[1] = "Throw a snowball at them",
+						[2] = "Say sorry",
+						[3] = "Lick their snowmobile",
+					},
+					["responses"] = {
+						[1] = "Woah there bud, that was awful rude of you, eh.",
+						[2] = "Oh wow, I haven't heard a quality sorry like that in a while. Thanks, I needed that.",
+						[3] = "Excuse me there, bud. Would you mind backing away from the snowmobile.",
+					},
+					["outcomes"] = {
+						[1] = "negative",
+						[2] = "positive",
+						[3] = "negative",
+					},
+				},
+			},
+		},
+	},
+	["level5"] = {
+		[1] = {
+			["intro"] = "Another person, I haven't seen another person in months. Are you looking for the Great Refrigerator too? I've just aboot run oot of supplies looking for it. What can I do for ya?",
+			["success"] = "Well, I am almost out of food, and will probably die if I give these to you, but you seem like a nice guy. So here, have these timbits, and good luck!",
+			["fail"] = "Well, I've got nothing for you, but good luck looking for the Great Refrigerator.",
+			["minscore"] = 2,
+			["food"] = "",
+			["stages"] = {
+				[1] = {
+					["options"] = {
+						[1] = "Inform them that their search has been pointless",
+						[2] = "Applaud their tenacity to continue the search",
+						[3] = "Compliment their snowshoes",
+					},
+					["responses"] = {
+						[1] = "Ouch bud, don't ya know that the truth hurts...",
+						[2] = "Thanks bud, but I'm not sure how much I have left in me.",
+						[3] = "Oh these? Yeah I found them out by that tree over there. Seems like the previous owner got mauled by a polar bear. But hey, they seem to still be working just fine.",
+					},
+					["outcomes"] = {
+						[1] = "negative",
+						[2] = "positive",
+						[3] = "positive",
+					},
+				},
+				[2] = {
+					["options"] = {
+						[1] = "Offer to take over the search for them",
+						[2] = "Distract them with the classic look over there tactic",
+						[3] = "Throw a ski at them",
+					},
+					["responses"] = {
+						[1] = "Thanks bud, I was thinking about heading back anyway.",
+						[2] = "What? What's over... Oh, you sneaky hoser you.",
+						[3] = "Oi, what was that for, ya hoser?!",
+					},
+					["outcomes"] = {
+						[1] = "positive",
+						[2] = "negative",
+						[3] = "negative",
+					},
+				},
+			},
+		},
+	},
 	["template"] = {
 		[1] = {
 			["intro"] = "",
